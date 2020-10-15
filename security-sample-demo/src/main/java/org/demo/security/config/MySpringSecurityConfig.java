aeae760978563f2f64c50c7cdcc5829ecf53c136
zhanjiuzhan <1065205727@qq.com>
2020-10-16 00:38:06 +0800

    基于内存认证

--- security-sample-demo/src/main/java/org/demo/security/config/MySpringSecurityConfig.java
+++ security-sample-demo/src/main/java/org/demo/security/config/MySpringSecurityConfig.java
@@ -1,12 +1,19 @@
 package org.demo.security.config;
 
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.cglib.proxy.NoOp;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 /**
  * @author Administrator
  */
+@Configurable
 @Component
 public class MySpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -22,5 +29,18 @@ public class MySpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin().permitAll();
 
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("root1").password("abc123").roles("USER")
+            .and()
+            .withUser("root2").password("abc123").roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
